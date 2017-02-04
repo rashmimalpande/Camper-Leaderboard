4eb03a741aaae4bc1c1a75588ae8c93c55861d51
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-04 19:03:09 +0530

    Create a board with some initial data

---
+++ js/app.js
@@ -0,0 +1,54 @@
+var app =new Vue({
+    el: '.main',
+    data: {
+
+        players:[ 
+            {
+                name: 'Ted Mosby',
+                points: 100,
+                all: 500
+            },
+
+             {
+                name: 'Barney Stinson',
+                points: 250,
+                all: 250
+            },
+
+             {
+                name: 'Ross Gellar',
+                points: 300,
+                all: 700
+            },
+
+            {
+                name: 'Jesse Pinkman',
+                points: 402,
+                all: 500
+            },
+
+             {
+                name: 'Phoebe Buffay',
+                points: 500,
+                all: 500
+            },
+
+            {
+                name: 'Rachel Green',
+                points: 500,
+                all: 500
+            },
+
+             {
+                name: 'Monica Geller',
+                points: 700,
+                all: 700
+            }
+
+
+
+        
+        
+        ]
+    }
+})
