253ef5a2f280b9441aad59a2904ff8d15213468f
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-05 11:06:21 +0530

    Filter the points on click

--- js/app.js
+++ js/app.js
@@ -6,49 +6,69 @@ var app =new Vue({
             {
                 name: 'Ted Mosby',
                 points: 100,
-                all: 500
+                all: 1200
             },
 
              {
                 name: 'Barney Stinson',
-                points: 250,
-                all: 250
+                points: 350,
+                all: 3021
             },
 
              {
                 name: 'Ross Gellar',
                 points: 300,
-                all: 700
+                all: 5013
             },
 
             {
                 name: 'Jesse Pinkman',
                 points: 402,
-                all: 500
+                all: 2500
             },
 
              {
                 name: 'Phoebe Buffay',
-                points: 500,
-                all: 500
+                points: 200,
+                all: 2100
             },
 
             {
                 name: 'Rachel Green',
                 points: 500,
-                all: 500
+                all: 700
             },
 
              {
                 name: 'Monica Geller',
                 points: 700,
-                all: 700
+                all: 1000 
             }
-
-
-
-        
         
         ]
+    },
+
+    methods: {
+        filterMax30: function(){
+            var sorted=[];
+            var players = this.players;
+
+            sorted = players.sort(function(a, b){
+                return b.points - a.points;
+            });
+
+            return sorted;
+        },
+
+        filterAllTime: function(){
+            var sorted=[];
+            var players = this.players;
+
+            sorted = players.sort(function(a, b){
+                return b.all - a.all;
+            });
+
+            return sorted;
+        }
     }
 })
